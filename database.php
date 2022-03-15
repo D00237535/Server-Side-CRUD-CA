c02e5ab8b6dc9bd10002cf1bf45879c4f1e50dfd
D00237535 <D00237535@student.dkit.ie>
2022-03-15 11:57:01 +0000

    Final Commit

--- database.php
+++ database.php
@@ -3,6 +3,11 @@
     $username = 'D00237535';
     $password = 'MWKgSYa0';
 
+    // $dsn = 'mysql:host=localhost;dbname=ssd_ca2_db';
+    // $username = 'root';
+    // $password = '';
+
+
     try {
         $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
